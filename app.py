3c77bd452788534f781b9afc87a9376fca6574ff
rogeriorodbr <rogerio.rodrigues@pucpr.edu.br>
2022-10-28 00:46:57 +0000

    Alteracao

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "ola mundo"
+    return "Aula Devops"
+
+@app.route("/health")
+def health():
+    return "200"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3000, debug=True)
+    app.run(host="0.0.0.0", port=os.environ['APP_PORT'], debug=True)
