ecf282bc0dee10feaaa75f2767924ef53d18b9e6
rogeriorodbr <rogerio.rodrigues@pucpr.edu.br>
2022-11-03 23:24:43 +0000

    Add counter endpoint

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
-from flask import Flask
+from flask import Flask, jsonify
+from multiprocessing import Value
 import os
+
+counter = Value('i', 0)
 
 app = Flask(__name__)
 
@@ -9,6 +12,18 @@ def hello_world():
 
 @app.route("/health")
 def health():
+    return "200"
+
+@app.route('/counter')
+def index():
+    with counter.get_lock():
+        counter.value += 1
+        # save the value ASAP rather than passing to jsonify
+        # to keep lock time short
+        unique_count = counter.value
+
+    return str(unique_count)
+    
     return "200"
 
 
